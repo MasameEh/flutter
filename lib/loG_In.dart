7dc0ab9fc03d8660929eb91c2db3554928d6854a
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2022-08-14 09:28:57 +0200

    Update loG_In.dart

--- lib/loG_In.dart
+++ lib/loG_In.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-class LogInScreen extends StatelessWidget {
 
+class LogInScreen extends StatefulWidget {
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
   var emailController = TextEditingController();
+
   var passwordController = TextEditingController();
 
+  var formKey = GlobalKey<FormState>();
+
+  bool isPassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,101 +26,131 @@ class LogInScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment:CrossAxisAlignment.start ,
-              children:[
-                const Text(
-                  'Log in',
-                  style: TextStyle(
-                    fontSize: 35.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress ,
-                  onFieldSubmitted: (value){
-                    print(value);
-                  },
-                  onChanged: (value){
-                    print(value);
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Email Adress',
-                    labelStyle: TextStyle(fontSize: 15.0, fontStyle: FontStyle.italic, ),
-                    prefixIcon: Icon(
-                      Icons.email,
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment:CrossAxisAlignment.start ,
+                children:[
+                   Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
-                    border: OutlineInputBorder(),
                   ),
-                ),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  keyboardType: TextInputType.visiblePassword,
-                  onFieldSubmitted: (value){
-                    print(value);
-                  },
-                  onChanged: (value){
-                    print(value);
-                  },
-                  decoration: const InputDecoration(
-                    labelText:'Password' ,
-                    border: OutlineInputBorder(),
-                    labelStyle: TextStyle(fontSize: 15.0, fontStyle: FontStyle.italic, ),
-                    prefix: Icon(
-                      Icons.lock,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.remove_red_eye,),
-
+                  SizedBox(
+                    height: 20.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: double.infinity,
-                  child: MaterialButton(
-                    onPressed:(){
-                      print(emailController.text);
-                      print(passwordController.text);
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress ,
+                    onFieldSubmitted: (String? value){
+                      print(value);
                     },
-                    child: const Text(
-                      'Log In',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
+                    onChanged: (value){
+                      print(value);
+                    },
+                    validator: (value)
+                    {
+                      if (value == null || value.isEmpty)
+                      {
+                        return'email must be not empty';
+                      }
+                      return null;
+
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Email Adress',
+                      labelStyle: TextStyle(fontSize: 15.0, fontStyle: FontStyle.italic, ),
+                      prefixIcon: Icon(
+                        Icons.email,
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: isPassword,
+                    keyboardType: TextInputType.visiblePassword,
+                    onFieldSubmitted: (value){
+                      print(value);
+                    },
+                    onChanged: (value){
+                      print(value);
+                    },
+                    validator: (value)
+                    {
+                      if (value == null || value.isEmpty)
+                      {
+                        return 'password must be not empty';
+                      }
+                      return null;
+
+                    },
+                    decoration: InputDecoration(
+                      labelText:'Password' ,
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(fontSize: 15.0, fontStyle: FontStyle.italic, ),
+                      prefix: Icon(
+                        Icons.lock,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          isPassword ? Icons.visibility : Icons.visibility_off, ),
+                        onPressed: () {
+                          setState(() {
+                            isPassword = ! isPassword;
+                          });
+                      },
+                      ),
+
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: double.infinity,
+                    child: MaterialButton(
+                      onPressed:() {
+                        if (formKey.currentState!.validate()) {
+                          print(emailController.text);
+                          print(passwordController.text);
+                        }
+                      }, 
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Row(
-                  mainAxisAlignment:MainAxisAlignment.center,
-                  children: [
-                    Text('Don\'t have an account?'),
-                    TextButton(
-                      onPressed: (){},
-                      child: Text(
-                        'Register Now',
-                      ),
-                    )
-                  ],
-                ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    mainAxisAlignment:MainAxisAlignment.center,
+                    children: [
+                      Text('Don\'t have an account?'),
+                      TextButton(
+                        onPressed: (){},
+                        child: Text(
+                          'Register Now',
+                        ),
+                      )
+                    ],
+                  ),
 
-              ],
+                ],
 
+              ),
             ),
           ),
         ),
