ede712d8d3d1c4cc57db562d7793d4139b94716d
MasameEh <samehms116@gmail.com>
2022-09-22 06:03:58 +0200

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_first/layout/home_layout.dart';
 import 'package:my_first/modules/bmi/BMI_screen.dart';
-import 'package:my_first/messenger_with_list.dart';
+import 'package:my_first/modules/messenger/messenger_with_list.dart';
 import 'package:my_first/modules/bmi/result_screen.dart';
-import 'loG_In.dart';
-import 'messenger_screen.dart';
+import 'modules/login/loG_In.dart';
+import 'modules/messenger/messenger_screen.dart';
 
 
 
